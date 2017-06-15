20683ac6351e88d1eb3b4c1b9596d634d8085c35
Ivan_Burkat <Ivan_Burkat@EPBYMINW2024.minsk.epam.com>
2017-06-15 12:16:30 +0300

    1

--- src/com/iburkat/pyramid/entity/Pioint.java
+++ src/com/iburkat/pyramid/entity/Pioint.java
@@ -5,21 +5,38 @@ public class Pioint {
 	private int y;
 	private int z;
 	
+	public void setPoint(int osX, int osY, int osZ){
+		x = osX;
+		y = osY;
+		z = osZ;
+	}
+	
+	public void Point(int osX, int osY, int osZ) {
+		x = osX;
+		y = osY;
+		z = osZ;
+    }  
+
 	public int getX() {
 		return x;
 	}
+
 	public void setX(int x) {
 		this.x = x;
 	}
+
 	public int getY() {
 		return y;
 	}
+
 	public void setY(int y) {
 		this.y = y;
 	}
+
 	public int getZ() {
 		return z;
 	}
+
 	public void setZ(int z) {
 		this.z = z;
 	}

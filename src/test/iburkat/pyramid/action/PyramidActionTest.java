f5bcb0e316f3682dc6ddb73c6c003e9564130a76
Ivan_Burkat <Ivan_Burkat@EPBYMINW0825.minsk.epam.com>
2017-07-03 20:24:10 +0300

    11

---
+++ src/test/iburkat/pyramid/action/PyramidActionTest.java
@@ -0,0 +1,12 @@
+package test.iburkat.pyramid.action;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.iburkat.pyramid.entity.Pyramid;
+import com.iburkat.pyramid.action;
+
+
+public class PyramidActionTest {
+
+}
--- src/test/iburkat/pyramid/entity/PyramidTest.java
+++
@@ -1,5 +0,0 @@
-package test.iburkat.pyramid.entity;
-
-public class PyramidTest {
-
-}

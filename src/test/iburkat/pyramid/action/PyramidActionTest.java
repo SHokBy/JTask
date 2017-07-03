f373a7fd3889fe35ece6d83cd6c129b877ad49a6
Ivan_Burkat <Ivan_Burkat@EPBYMINW0825.minsk.epam.com>
2017-07-03 20:24:18 +0300

    11

--- src/test/iburkat/pyramid/action/PyramidActionTest.java
+++ src/test/iburkat/pyramid/action/PyramidActionTest.java
@@ -3,10 +3,16 @@ package test.iburkat.pyramid.action;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.iburkat.pyramid.entity.Point;
 import com.iburkat.pyramid.entity.Pyramid;
 import com.iburkat.pyramid.action;
 
 
 public class PyramidActionTest {
 
+	@Test
+	public void calculatePyramidVolume() throws Exception{
+		Point[] imputData = 
+		
+	}
 }
